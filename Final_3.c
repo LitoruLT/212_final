11aa1d6ed459bd99a016cbbab8652f7ba2adb853
Litoru <puwarin2001@gmail.com>
2021-03-29 18:12:39 +0700

    3-2

--- Final_3.c
+++ Final_3.c
@@ -19,19 +19,62 @@ void main()
             {
                 if(k==0)
                 {
-                    scanf("%d",first[i][j])
+                    scanf("%d",&first[i][j]);
                 }
                 else if(k==1)
                 {
-                    scanf("%d",sec[i][j])
+                    scanf("%d",&sec[i][j]);
                 }
                 else
                 {
-                    scanf("%d",third[i][j])
+                    scanf("%d",&third[i][j]);
                 }
             }
         }
         printf("\n");
     }
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    int sums1[(n[0]*2)+2];
+    int sums2[(n[1]*2)+2];
+    int sums3[(n[2]*2)+2];
+    for(i<=0 ; (i<(n[0]*2)+2) ; i++)
+    {
+        sums1[i]=0;
+    }
+    for(i<=0 ; (i<(n[1]*2)+2) ; i++)
+    {
+        sums2[i]=0;
+    }
+    for(i<=0 ; (i<(n[2]*2)+2) ; i++)
+    {
+        sums3[i]=0;
+    }
+
+
+    for(k=0; k<3 ;k++)
+    {
+        for(i=0; i<n[k] ;i++)
+        {
+            for(j=0; j<n[k] ;j++)
+            {
+                if(k==0)
+                {
+                    sums=sums+first[i][j];
+                }
+                else if(k==1)
+                {
+                    scanf("%d",&sec[i][j]);
+                }
+                else
+                {
+                    scanf("%d",&third[i][j]);
+                }
+            }
+        }
+        printf("\n");
+    }
+
 
 }
