9933c227fc3fe863198ce7c37289cf08dcae2720
Litoru <puwarin2001@gmail.com>
2021-03-29 19:28:04 +0700

    1_2 -1

--- Final_1_2.c
+++ Final_1_2.c
@@ -8,8 +8,55 @@ void main()
     n=get[0];
     c=get[1];
 
-    long long int nsave=n,csave=c,nuse=n,cuse=c;
-    while()
+    long long int nsave=n,csave=c,nuse=n,cuse=c,ntemp=1,ctemp=1,countn=0,countc=0;
+    while(nuse)
+    {
+        nuse=nuse/10;
+        ntemp=ntemp*10;
+        countn++;
+    }
+
+    ntemp=ntemp/10;
+    long long int seperateN[countn];
+    while(cuse)
+    {
+        cuse=cuse/10;
+        ctemp=ctemp*10;
+        countc++;
+    }
+    ctemp=ctemp/10;
+    long long int seperateC[countc];
+    nuse=n;cuse=c;
+
+    i=0;
+    while(ntemp)
+    {
+        ////////////printf("nuse = %lli , ntemp = %lli \n",nuse,ntemp);
+        seperateN[i]=nuse/ntemp;
+        nuse=nuse/10;
+        ntemp=ntemp/10;
+        i++;
+    }
+    i=0;
+    while(ctemp)
+    {
+        seperateC[i]=cuse/ctemp;
+        cuse=cuse/10;
+        ctemp=ctemp/10;
+        i++;
+    }
+    countc=countc-1;
+    countn=countn-1;
+    long long int check=0;
+    //while(!check)
+    {
+
+    }
+
+    for(i=0; i<countc ;i++)
+    {
+        printf("%lli ",seperateC[i]);
+    }
 
 
 
@@ -18,12 +65,9 @@ void main()
 
 
 
-
-
-
-
-
-    mul=1
+    if(0)
+    {
+    mul=1;
     while(mul<=c)
     {
         mul=mul*10;
@@ -45,6 +89,7 @@ void main()
         }
 
 
+    }
     }
     printf("%lli",k);
 }
