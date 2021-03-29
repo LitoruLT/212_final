9a125e4e495da7980bc8d5ce29c8bea1e940b903
Litoru <puwarin2001@gmail.com>
2021-03-29 19:07:18 +0700

    Recode ข้อ 1 ในไฟล์ Final_1_2

---
+++ Final_1_2.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+void main()
+{
+    long long int get[2],i,n,k,c,mul=1,temp;
+    for(i=0; i<2 ; i++)
+        scanf("%lli",&get[i]);
+    n=get[0];
+    c=get[1];
+
+    long long int nsave=n,csave=c,nuse=n,cuse=c;
+    while()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    mul=1
+    while(mul<=c)
+    {
+        mul=mul*10;
+        //printf("mul = %lli\n",mul);
+    }
+    k=n;
+    while(1)
+    {
+        k++;
+        if(k%mul==c)
+        {
+            //printf("k=%lli\n",k);
+            break;
+        }
+        else
+        {
+            //printf("At Con k=%lli\n",k);
+            continue;
+        }
+
+
+    }
+    printf("%lli",k);
+}
